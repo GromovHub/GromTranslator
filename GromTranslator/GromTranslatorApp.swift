88a6e4b1db652afaf511553b45854f91af2d9fed
Vitaly Gromov <gromovitaly@gmail.com>
2023-02-24 11:37:25 +0300

    Initial Commit

---
+++ GromTranslator/GromTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GromTranslatorApp.swift
+//  GromTranslator
+//
+//  Created by Vitaly Gromov on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct GromTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
